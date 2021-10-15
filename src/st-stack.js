7b8af6bfbd5de577fe0738830907c5ec2ab92148
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-15 18:20:00 +0300

    feat: add task3

--- src/st-stack.js
+++ src/st-stack.js
@@ -28,3 +28,87 @@ const { NotImplementedError } = require('../extensions/index.js');
     // remove line with error and write your code here
   }
 }
+// Linked List
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+// Stack implemented using LinkedList
+function Stack() {
+  this.top = null;
+}
+
+Stack.prototype.push = function(data) {
+  var newNode = new Node(data);
+
+  newNode.next = this.top; //Special attention
+  this.top = newNode;
+}
+
+Stack.prototype.pop = function() {
+  if (this.top !== null) {
+    var topItem = this.top.data;
+    this.top = this.top.next;
+    return topItem;
+  }
+  return null;
+}
+
+Stack.prototype.print = function() {
+  var curr = this.top;
+  while (curr) {
+    console.log(curr.data);
+    curr = curr.next;
+  }
+}
+
+// var stack = new Stack();
+// stack.push(3);
+// stack.push(5);
+// stack.push(7);
+// stack.print();
+
+// Queue implemented using LinkedList
+function Queue() {
+  this.head = null;
+  this.tail = null;
+}
+
+Queue.prototype.enqueue = function(data) {
+  var newNode = new Node(data);
+
+  if (this.head === null) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+}
+
+Queue.prototype.dequeue = function() {
+  var newNode;
+  if (this.head !== null) {
+    newNode = this.head.data;
+    this.head = this.head.next;
+  }
+  return newNode;
+}
+
+Queue.prototype.print = function() {
+  var curr = this.head;
+  while (curr) {
+    console.log(curr.data);
+    curr = curr.next;
+  }
+}
+
+var queue = new Queue();
+queue.enqueue(3);
+queue.enqueue(5);
+queue.enqueue(7);
+queue.print();
+queue.dequeue();
+queue.dequeue();
+queue.print();
