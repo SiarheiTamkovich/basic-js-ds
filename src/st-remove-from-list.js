92e4f8eebb811aeb63be483ba426d084b1422abd
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-15 17:30:04 +0300

    feat: add task 2

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,27 +19,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  function removeKFromList(l, k) {
-    
-    let head = l
-    let previous = null
-  
-    while(l){
-      if(l.value === k){            
-        if(previous===null){
-            head = l = l.next
-            continue
-        } else {
-            previous.next = l.next
-            l = l.next
-            continue
-        }
+module.exports = function removeKFromList(l, k ) {
+   
+  let head = l
+  let previous = null
+
+  while(l){
+    if(l.value === k){            
+      if(previous===null){
+          head = l = l.next
+          continue
+      } else {
+          previous.next = l.next
+          l = l.next
+          continue
       }
-      previous = l
-      l = l.next
     }
-    return head
+    previous = l
+    l = l.next
   }
+  return head
 }
+  
