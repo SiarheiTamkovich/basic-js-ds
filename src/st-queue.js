3fbe1fca3c8348e5b608bd30ca43a8bd7b6c1680
SiarheiTamkovich <mpg2000@mail.ru>
2021-10-15 20:45:29 +0300

    feat: add task4

--- src/st-queue.js
+++ src/st-queue.js
@@ -31,3 +31,58 @@ module.exports = class Queue {
   }
 
 }
+
+class QNode
+{
+    constructor(key)
+    {
+        this.key = key;
+        this.next = null;
+    }
+}
+ 
+let front = null, rear = null;
+ 
+function enqueue(key)
+{
+    // Create a new LL node
+        let temp = new QNode(key);
+   
+        // If queue is empty, then new node is front and rear both
+        if (rear == null) {
+            front = rear = temp;
+            return;
+        }
+   
+        // Add the new node at the end of queue and change rear
+        rear.next = temp;
+        rear = temp;
+}
+ 
+ 
+function dequeue()
+{
+    // If queue is empty, return NULL.
+        if (front == null)
+            return;
+   
+        // Store previous front and move front one node ahead
+        let temp = front;
+        front = front.next;
+   
+        // If front becomes NULL, then change rear also as NULL
+        if (front == null)
+            rear = null;
+}
+ 
+ 
+enqueue(10);
+enqueue(20);
+dequeue();
+dequeue();
+enqueue(30);
+enqueue(40);
+enqueue(50);
+dequeue();
+document.write("Queue Front : " + front.key+"<br>");
+document.write("Queue Rear : " + rear.key+"<br>");
